e250ba09b342dd96f266c34e547e2654ad98b969
santo <santoshvijaypawar@gmail.com>
2016-08-10 14:43:00 +0530

    Create module1.js

---
+++ module-export/module1.js
@@ -0,0 +1,9 @@
+function myFunction() {
+	console.log('function was called');
+	// body...
+}
+
+var myString='String this is ';
+
+module.exports.myFunction=myFunction;
+module.exports.myString=myString;
