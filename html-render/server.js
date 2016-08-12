668c41834758349cd6c134bbf0300c4dfc2d9db1
santo <santoshvijaypawar@gmail.com>
2016-08-12 14:36:30 +0530

    node fs

--- html-render/server.js
+++ html-render/server.js
@@ -1,1 +1,18 @@
 
+var http= require('http');
+var fs=require('fs');
+function onRequest(req,res){
+  res.writeHead(200,{'content-type':'text/html'});
+  fs.readFile('./index.html','null',function(error,data){
+    if(error){
+      res.write(401);
+      res.end();
+    }else{
+      res.write(data)
+    }
+  });
+}
+
+
+
+http.createServer(onRequest).listen(8000);
